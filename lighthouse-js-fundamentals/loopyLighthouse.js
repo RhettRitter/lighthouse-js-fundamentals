1645ecc32a93365222fbd7d023fafa8cec05acf8
Rhett Ritter <rhett.ritter@hotmail.com>
2022-03-08 19:05:27 +0000

    commit

---
+++ lighthouse-js-fundamentals/loopyLighthouse.js
@@ -0,0 +1,9 @@
+for (var x = 100; x > 99 && x < 201; x++){
+    if(x % 3 === 0 && x % 4 === 0){
+        console.log('LoopyLighthouse');}
+   else if(x % 3 === 0){
+        console.log('Loopy');}
+   else if(x % 4 === 0){
+        console.log('Lighthouse');}
+        else console.log(x)
+   }
